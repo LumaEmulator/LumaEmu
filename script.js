4e5639eb1ad671dd43c5bdbc3e7aa26aea1bcab6
LumaEmulator <155094544+LumaEmulator@users.noreply.github.com>
2024-01-15 11:12:57 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+//Code From https://developers.google.com/web/fundamentals/app-install-banners/native
+
+const installBtn = document.querySelector('#installBtn');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  installBtn.style.display = 'inline-block';
+});
+
+installBtn.addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  installBtn.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
+
+window.onload = function() {
+  var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+  if (!isMobile) {
+    document.getElementById('content').style.display = 'none';
+    document.getElementById('computerNotSupported').style.display = 'block';
+  }
+}
